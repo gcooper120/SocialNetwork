ef4ef249d1dd42c9beb176321f53f33183067750
George <cooper.george120@gmail.com>
2018-04-15 20:50:54 -0400

    Added mysql

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var express 		= require("express"),
 		app 				= express(),
-		bodyParser 	= require("body-parser");
+		bodyParser 	= require("body-parser"),
+		mysql				= require("mysql");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//mySQL connection
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'Social Network'
+});
 
+connection.connect()
+
+connection.query('SELECT * from user', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('Successfully connected to db if "First user" shows up.: ', rows[0].about)
+})
+
+connection.end()
 
 // Landing Page route
 app.get("/", function(req,res){
