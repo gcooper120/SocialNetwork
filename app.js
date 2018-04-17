280f29dbc59d49676df59012c3c3357b09c7a58d
George <cooper.george120@gmail.com>
2018-04-17 12:00:34 -0400

    Authentication working

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
-var express 		= require("express"),
-		app 				= express(),
-		bodyParser 	= require("body-parser"),
-		mysql				= require("mysql");
+var express 			= require("express"),
+		app 					= express(),
+		passport			= require("passport"),
+		session				= require("express-session"),
+		bodyParser 		= require("body-parser"),
+		mysql					= require("mysql2");
+		env						= require('dotenv').load(),
+		exphbs 				= require('express-handlebars');
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+//Handlebars config
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
-//mySQL connection
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'Social Network'
-});
+//BodyParser Config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-connection.connect()
 
-connection.query('SELECT * from user', function (err, rows, fields) {
-  if (err) throw err
+//Passport Configuration
+app.use(session({ secret: 'adsvokw0942nv9204231va2',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-  console.log('Successfully connected to db if "First user" shows up.: ', rows[0].about)
-})
+//Models
+var models = require("./app/models");
 
-connection.end()
+//Routes
+var authRoute = require('./app/routes/auth.js')(app, passport);
 
-// Landing Page route
-app.get("/", function(req,res){
-	res.render("landing");
+//load Passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
 });
 
 
 
 //Listening on port 3000
-app.listen(3000, function(){
-	console.log("Starting Server");
+app.listen(4000, function(err){
+	if (!err) {
+		console.log("Starting server on port 4000");
+		console.log("This is the " + app.get('env') + " environment.");
+	}
+	else console.log(err)
 });
